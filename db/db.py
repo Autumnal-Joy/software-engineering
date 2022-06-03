939b59c45d339db37dd3e714c3a798b112cfedd9
Love a wild horse <1350854515@qq.com>
2022-06-03 15:22:17 +0800

    增加了几个对数据库需要提供接口的描述

--- db/db.py
+++ db/db.py
@@ -1,3 +1,15 @@
 class DB:
     def __init__(self, path):
         pass
+
+    def Query(tablename: str, key: str) -> dict:
+        return {"username": "郭阳", "password": "123456"}
+
+    def Insert(tablename: str, key: str, val: dict) -> bool:
+        return True
+
+    def Delete(tablename: str, key: str) -> bool:
+        return True
+
+    def Update(tablename: str, key: str, val: dict) -> bool:
+        return True
