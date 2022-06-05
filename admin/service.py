acd16aff21620383310ca69d095829a177b1a713
KingOfDings <81497243+KingOfDings@users.noreply.github.com>
2022-06-05 22:10:20 +0800

    Update service.py

--- admin/service.py
+++ admin/service.py
@@ -1,6 +1,48 @@
+import sys
+
+sys.path.append("..")
+import json
+import time
+import threading
+from structure import Order
+from structure import WaitArea
+from structure import ChargeBoot
+
+FPN = 0  # FastCharingPileNum(FPN)        快充电桩数
+TPN = 0  # TrickleChargingPileNum(TPN)    慢充电桩数
+N = 0  # WaitingAreaSize(N)           等候区车位容量
+M = 0  # ChargingQueueLen(M)          充电桩排队队列长度
+
+
 class Service:
-    def __init__(self, db , pd):
+    def __init__(self, db, pd):
         self.db = db
+        self.N, self.M, self.FPN, self.TPN = pd.N, pd.M, pd.FPN, pd.TPN
+        self.usr2ord = pd.usr2ord  # username->Order
+        self.usr2bill = pd.usr2bill  # username->[Bill]
+        self.mutex_wait_lock = pd.mutex_wait_lock
+        self.waitqueue = pd.waitqueue
+        self.fast_ready_lock = pd.fast_ready_lock
+        self.slow_ready_lock = pd.slow_ready_lock
+        self.FastReadyQueue = pd.FastReadyQueue
+        self.SlowReadyQueue = pd.SlowReadyQueue
+        self.FastBoot = pd.FastBoot
+        self.SlowBoot = pd.SlowBoot
+        self.Fast_Speed = pd.Fast_Speed
+        self.Slow_Speed = pd.Slow_Speed
+        self.Schedule = pd.Schedule
+
+    def debug(self):
+        print("userord:", self.usr2ord)
+        print("userbill:", self.usr2bill)
+        print("waitqueue:", self.waitqueue)
+        print("FastReadyQueue:", self.FastReadyQueue)
+        print("SlowReadyQueue:", self.SlowReadyQueue)
+        print("FastBoot:", self.FastBoot)
+        print("SlowBoot:", self.SlowBoot)
+        print("Schedule:", self.Schedule)
+        return 1
+
     """
     params
         username            用户名
@@ -11,16 +53,22 @@ class Service:
 
     """
 
-    def adminLogin(self, username, password):
-        # data, err = None, "用户不存在"
-        data, err = {
-                        "status": True
-                    }, None
+    def adminLogin(self, username: str, password: str):
+        # data, err = None, "管理员用户不存在"
+        data, err = {"status": False}, None
+        table = self.db.Query("AdminInfo", username)
+        if not table:  # 字典为空
+            err = "用户不存在"
+        elif table["password"] != password:
+            err = "用户名或密码错误"
+        else:
+            data = {"status": True}
         return data, err
 
     """
     params
         username            用户名
+
     returns
         chargerID           充电桩编号
         working             充电桩是否启动
@@ -29,47 +77,63 @@ class Service:
         totalChargeQuantity 系统启动后充电桩总计充电量（度）
     """
 
-    def adminGetChargers(self, username):
-        data, err = [{
-            "chargerID": 1,
-            "working": True,
-            "totalChargeCount": 30,
-            "totalChargeTime": 123456789,
-            "totalChargeQuantity": 120
-        }, {
-            "chargerID": 2,
-            "working": False,
-            "totalChargeCount": 12,
-            "totalChargeTime": 123456789,
-            "totalChargeQuantity": 120
-        }, {
-            "chargerID": 3,
-            "working": False,
-            "totalChargeCount": 0,
-            "totalChargeTime": 0,
-            "totalChargeQuantity": 0
-        }], None
-
+    def adminGetChargers(self, username: str):
+        data, err = [], None
+        BootList = []
+        BootList.extend(self.FastBoot)
+        BootList.extend(self.SlowBoot)
+        # print("*******F", len(self.FastBoot))
+        # print("*******T", len(self.SlowBoot))
+        # print("*******b", len(BootList))
+        for boot in BootList:
+            table = self.db.Query("ChargerBillList", boot.name)
+            totalChargeCount = 0
+            totalChargeTime = 0
+            totalChargeQuantity = 0
+            for i in table:
+                bill = table[i]
+                totalChargeCount = totalChargeCount + 1
+                totalChargeTime = totalChargeTime + (bill.end - bill.start) * 100  # 需修改，*100
+                totalChargeQuantity = totalChargeQuantity + bill.real_quantity
+                # print("**********totalChargestart", bill.start)
+                # print("**********totalChargestart", bill.end)
+            # print("**********totalChargeTime",totalChargeTime)
+            Info = {"chargerID": boot.name, "working": boot.working, "totalChargeCount": totalChargeCount,
+                    "totalChargeTime": totalChargeTime, "totalChargeQuantity": totalChargeQuantity}
+            data.append(Info)
         return data, err
 
     """
     params
         username            用户名
         chargerID           充电桩编号
-        turn                启动或关闭充电桩（"on" or "off"）
+        turn                启动或关闭充电桩（"on" , "off"）
 
     returns
         status              用于没有实际数据返回时，表示操作是否成功
 
     """
 
-    def adminTurnCharger(self, username, chargerID, turn):
-        data, err = {
-                        "status": True
-                    }, None
+    def adminTurnCharger(self, username: str, chargerID, turn):
+        # print("#########turn",turn)
+        # print("#########chargerID",int(chargerID[1:]))
+        # print("#########test", self.FastBoot[int(chargerID[1:])])
+        data, err = {"status": True}, None
+        table = self.db.Query("ChargerInfo", None)
+        if chargerID[0] == 'T':
+            if turn == "off":
+                self.SlowBoot[int(chargerID[1:])].shut()
+                print("******workingstate", self.SlowBoot[int(chargerID[1:])].working)
+            else:
+                self.SlowBoot[int(chargerID[1:])].start()
+        else:
+            if turn == "off":
+                self.FastBoot[int(chargerID[1:])].shut()
+            else:
+                self.FastBoot[int(chargerID[1:])].start()
         return data, err
 
-    """
+    """    
     params
         username            用户名
         chargerID           充电桩编号
@@ -80,21 +144,49 @@ class Service:
         waitingTime         用户等待时间（毫秒时间戳）
     """
 
+    # 临时用
+    def adminGetUsersInterface(self, chargerID: int):
+        if chargerID == 1:
+            return [{
+                "username": "walker",
+                "chargeQuantity": 24,
+                "waitingTime": 12345678
+            }, {
+                "username": "AutJ",
+                "chargeQuantity": 32,
+                "waitingTime": 23456781
+            }]
+        else:
+            return None
+
     def adminGetUsers(self, username, chargerID):
-        data, err = [{
-            "username": "walker",
-            "chargeQuantity": 24,
-            "waitingTime": 12345678
-        }, {
-            "username": "AutJ",
-            "chargeQuantity": 32,
-            "waitingTime": 23456781
-        }], None
+        data, err = [], None
+        if chargerID[0] == 'T':
+            boot = self.SlowBoot[int(chargerID[1:])]
+        else:
+            boot = self.FastBoot[int(chargerID[1:])]
+        table = boot.get_all_ord_now()
+        for ord in table:
+            # ord.show()
+            info = {}
+            info["username"] = ord.username
+            info["chargeQuantity"] = ord.chargeQuantity
+            now = time.time()
+            begin = ord.createtime
+            info["waitingTime"] = (now - begin) * 1000
+            data.append(info)
+            # print("******data", info)
+            # print("***now", now)
+            # print("***begin", begin)
+            # print("waittimg",(now - begin) / 3600)
+        # print("*******table", table)
+        # print("******data",data)
         return data, err
 
     """
     params
         username            用户名
+
     returns
         time                报表对应的时间（"今日" or "本周" or "本月" or...）
         totalChargeCount    报表时间对应的所有充电桩累计充电次数
@@ -114,57 +206,77 @@ class Service:
         }
     """
 
-    def adminGetTable(self, username):
-        data, err = [{
-            "time": "今日",
-            "totalChargeCount": 30,
-            "totalChargeTime": 99999999,
-            "totalChargeQuantity": 320,
-            "totalChargeCost": 1100,
-            "totalServiceCost": 600,
-            "totalCost": 1700,
-            "chargers": [{
-                "chargerID": 1,
-                "totalChargeCount": 10,
-                "totalChargeTime": 12345678,
-                "totalChargeQuantity": 120,
-                "totalChargeCost": 100,
-                "totalServiceCost": 100,
-                "totalCost": 200
-            }, {
-                "chargerID": 2,
-                "totalChargeCount": 20,
-                "totalChargeTime": 87654321,
-                "totalChargeQuantity": 200,
-                "totalChargeCost": 1000,
-                "totalServiceCost": 500,
-                "totalCost": 1500
-            }]
-        }, {
-            "time": "本周",
-            "totalChargeCount": 30,
-            "totalChargeTime": 99999999,
-            "totalChargeQuantity": 320,
-            "totalChargeCost": 1100,
-            "totalServiceCost": 600,
-            "totalCost": 1700,
-            "chargers": [{
-                "chargerID": 1,
-                "totalChargeCount": 10,
-                "totalChargeTime": 12345678,
-                "totalChargeQuantity": 120,
-                "totalChargeCost": 100,
-                "totalServiceCost": 100,
-                "totalCost": 200
-            }, {
-                "chargerID": 2,
-                "totalChargeCount": 20,
-                "totalChargeTime": 87654321,
-                "totalChargeQuantity": 200,
-                "totalChargeCost": 1000,
-                "totalServiceCost": 500,
-                "totalCost": 1500
-            }]
-        }
-                    ], None
+    def tableAdd(self,table: dict, add: dict):
+        table["totalChargeCount"] = table["totalChargeCount"] + 1
+        table["totalChargeTime"] = table["totalChargeTime"] + add["ChargeTime"]
+        table["totalChargeQuantity"] = table["totalChargeQuantity"]  + add["ChargeQuantity"]
+        table["totalChargeCost"] = table["totalChargeCost"] + add["ChargeCost"]
+        table["totalServiceCost"] = table["totalServiceCost"] + add["ServiceCost"]
+        table["totalCost"] = table["totalServiceCost"] + table["totalChargeCost"]
+        return table
+
+    def adminGetTable(self, username: str):
+        print("********now", 1)
+        data, err = [], None
+        BootList = []
+        BootList.extend(self.FastBoot)
+        BootList.extend(self.SlowBoot)
+
+
+        billday = {"time": "今日", "totalChargeCount": 0,"totalChargeTime": 0,"totalChargeQuantity": 0,
+                   "totalChargeCost": 0,"totalServiceCost": 0,"totalCost": 0,"chargers":[]}
+        billweek = {"time": "本周", "totalChargeCount": 0,"totalChargeTime": 0,"totalChargeQuantity": 0,
+                   "totalChargeCost": 0,"totalServiceCost": 0,"totalCost": 0,"chargers":[]}
+        billmonth = {"time": "本月", "totalChargeCount": 0,"totalChargeTime": 0,"totalChargeQuantity": 0,
+                   "totalChargeCost": 0,"totalServiceCost": 0,"totalCost": 0,"chargers":[]}
+        billall = {"time": "总共", "totalChargeCount": 0,"totalChargeTime": 0,"totalChargeQuantity": 0,
+                   "totalChargeCost": 0,"totalServiceCost": 0,"totalCost": 0,"chargers":[]}
+        bootbilltable = {}
+        now = time.time()
+
+        for boot in BootList:
+            table = self.db.Query("ChargerBillList", boot.name)
+            bootbilltable[boot.name] = {"day": {"chargerID": boot.name, "totalChargeCount": 0, "totalChargeTime": 0, "totalChargeQuantity": 0,
+                                                "totalChargeCost": 0, "totalServiceCost": 0, "totalCost": 0},
+                                        "week": {"chargerID": boot.name,"totalChargeCount": 0, "totalChargeTime": 0, "totalChargeQuantity": 0,
+                                                "totalChargeCost": 0, "totalServiceCost": 0, "totalCost": 0},
+                                        "month": {"chargerID": boot.name,"totalChargeCount": 0, "totalChargeTime": 0, "totalChargeQuantity": 0,
+                                                "totalChargeCost": 0, "totalServiceCost": 0, "totalCost": 0},
+                                        "all": {"chargerID": boot.name,"totalChargeCount": 0, "totalChargeTime": 0, "totalChargeQuantity": 0,
+                                                "totalChargeCost": 0, "totalServiceCost": 0, "totalCost": 0}
+                                        }
+            for i in table:
+                bill = table[i]
+                bill.Show()                               # 需修改，/ 3600
+                addDict = {"ChargeTime": (bill.end - bill.start) / 3600,"ChargeQuantity": bill.real_quantity,
+                           "ChargeCost": bill.chargecost,"ServiceCost": bill.servecost
+                          }
+                passtime = now - bill.billTime / 1000
+                if passtime < 86400:
+                    billday = self.tableAdd(billday, addDict)
+                    bootbilltable[boot.name]["day"] = self.tableAdd(bootbilltable[boot.name]["day"], addDict)
+                if passtime < 604800:
+                    billweek = self.tableAdd(billweek, addDict)
+                    bootbilltable[boot.name]["week"] = self.tableAdd(bootbilltable[boot.name]["week"], addDict)
+                if passtime < 2592000:
+                    billmonth = self.tableAdd(billmonth, addDict)
+                    bootbilltable[boot.name]["month"] = self.tableAdd(bootbilltable[boot.name]["month"], addDict)
+                billall = self.tableAdd(billall, addDict)
+                bootbilltable[boot.name]["all"] = self.tableAdd(bootbilltable[boot.name]["all"], addDict)
+
+                # print("**********totalChargestart", bill.start)
+                # print("**********totalChargestart", bill.end)
+            # print("**********totalChargeTime",totalChargeTime)
+        for boot in BootList:
+            billday["chargers"].append(bootbilltable[boot.name]["day"])
+            billweek["chargers"].append(bootbilltable[boot.name]["week"])
+            billmonth["chargers"].append(bootbilltable[boot.name]["month"])
+            billall["chargers"].append(bootbilltable[boot.name]["all"])
+
+        print(billday)
+        print(billweek)
+        print(billmonth)
+        print(billall)
+
+        data = [billday, billweek, billmonth, billall]
         return data, err
