f15a8ebb59dc149f3b334db897cc7ae9b5f06af1
KingOfDings <81497243+KingOfDings@users.noreply.github.com>
2022-06-05 19:26:37 +0800

    Update service.py

--- admin/service.py
+++ admin/service.py
@@ -1,50 +1,6 @@
-import sys
-
-sys.path.append("..")
-import json
-import time
-import threading
-from structure import Order
-from structure import WaitArea
-from structure import ChargeBoot
-
-FPN = 0  # FastCharingPileNum(FPN)        快充电桩数
-TPN = 0  # TrickleChargingPileNum(TPN)    慢充电桩数
-N = 0  # WaitingAreaSize(N)           等候区车位容量
-M = 0  # ChargingQueueLen(M)          充电桩排队队列长度
-
-
 class Service:
-    def __init__(self, db ,pd):
+    def __init__(self, db , pd):
         self.db = db
-        self.N, self.M, self.FPN, self.TPN = pd.N, pd.M, pd.FPN, pd.TPN
-        self.usr2ord = pd.usr2ord  # username->Order
-        self.usr2bill = pd.usr2bill  # username->[Bill]
-        self.mutex_wait_lock = pd.mutex_wait_lock
-        self.waitqueue = pd.waitqueue
-        self.fast_ready_lock = pd.fast_ready_lock
-        self.slow_ready_lock = pd.slow_ready_lock
-        self.FastReadyQueue = pd.FastReadyQueue
-        self.SlowReadyQueue = pd.SlowReadyQueue
-        self.FastBoot = pd.FastBoot
-        self.SlowBoot = pd.SlowBoot
-        self.Fast_Speed = pd.Fast_Speed
-        self.Slow_Speed = pd.Slow_Speed
-        self.Schedule = pd.Schedule
-
-    def debug(self):
-        print("userord:",self.usr2ord)
-        print("userbill:", self.usr2bill)
-        print("waitqueue:", self.waitqueue)
-        print("FastReadyQueue:", self.FastReadyQueue)
-        print("SlowReadyQueue:", self.SlowReadyQueue)
-        print("FastBoot:", self.FastBoot)
-        print("SlowBoot:", self.SlowBoot)
-        print("Schedule:", self.Schedule)
-        return 1
-
-
-
     """
     params
         username            用户名
@@ -55,22 +11,16 @@ class Service:
 
     """
 
-    def adminLogin(self, username: str, password: str):
-        # data, err = None, "管理员用户不存在"
-        data, err = {"status": False}, None
-        table = self.db.Query("AdminInfo", username)
-        if not table:  # 字典为空
-            err = "用户不存在"
-        elif table["password"] != password:
-            err = "用户名或密码错误"
-        else:
-            data = {"status": True}
+    def adminLogin(self, username, password):
+        # data, err = None, "用户不存在"
+        data, err = {
+                        "status": True
+                    }, None
         return data, err
 
     """
     params
         username            用户名
-        
     returns
         chargerID           充电桩编号
         working             充电桩是否启动
@@ -79,43 +29,47 @@ class Service:
         totalChargeQuantity 系统启动后充电桩总计充电量（度）
     """
 
-    def adminGetChargers(self, username: str):
-        data, err = None, None
-        data = self.db.Query("ChargerInfo", None)
-        if not data:  # 列表为空
-            err = "查询不到充电桩"
+    def adminGetChargers(self, username):
+        data, err = [{
+            "chargerID": 1,
+            "working": True,
+            "totalChargeCount": 30,
+            "totalChargeTime": 123456789,
+            "totalChargeQuantity": 120
+        }, {
+            "chargerID": 2,
+            "working": False,
+            "totalChargeCount": 12,
+            "totalChargeTime": 123456789,
+            "totalChargeQuantity": 120
+        }, {
+            "chargerID": 3,
+            "working": False,
+            "totalChargeCount": 0,
+            "totalChargeTime": 0,
+            "totalChargeQuantity": 0
+        }], None
+
         return data, err
 
     """
     params
         username            用户名
         chargerID           充电桩编号
-        turn                启动或关闭充电桩（"on":True , "off":False）
+        turn                启动或关闭充电桩（"on" or "off"）
 
     returns
         status              用于没有实际数据返回时，表示操作是否成功
 
     """
 
-    def adminTurnCharger(self, username: str, chargerID, turn):
-        data, err = {"status": False}, None
-        table = self.db.Query("ChargerInfo", None)
-        if len(table) < chargerID:
-            err = "编号不存在，编号超过充电桩总数"
-        else:
-            newEntry = table[chargerID - 1]
-            if turn == "on":
-                newEntry["working"] = True
-            else:
-                newEntry["working"] = False
-            status = self.db.Update("ChargerInfo", chargerID, newEntry)
-            if status:
-                data = {"status": True}
-            else:
-                err = "更新数据库失败"
+    def adminTurnCharger(self, username, chargerID, turn):
+        data, err = {
+                        "status": True
+                    }, None
         return data, err
 
-    """    
+    """
     params
         username            用户名
         chargerID           充电桩编号
@@ -126,34 +80,21 @@ class Service:
         waitingTime         用户等待时间（毫秒时间戳）
     """
 
-    # 临时用
-    def adminGetUsersInterface(self, chargerID:int):
-        if chargerID == 1:
-            return [{
-                "username": "walker",
-                "chargeQuantity": 24,
-                "waitingTime": 12345678
-            }, {
-                "username": "AutJ",
-                "chargeQuantity": 32,
-                "waitingTime": 23456781
-            }]
-        else:
-            return None
-
     def adminGetUsers(self, username, chargerID):
-        data, err = None, None
-        id = int(chargerID)
-
-        data = self.adminGetUsersInterface(id)
-        if not data:
-            err = "充电桩编号不存在"
+        data, err = [{
+            "username": "walker",
+            "chargeQuantity": 24,
+            "waitingTime": 12345678
+        }, {
+            "username": "AutJ",
+            "chargeQuantity": 32,
+            "waitingTime": 23456781
+        }], None
         return data, err
 
     """
     params
         username            用户名
-
     returns
         time                报表对应的时间（"今日" or "本周" or "本月" or...）
         totalChargeCount    报表时间对应的所有充电桩累计充电次数
@@ -173,9 +114,57 @@ class Service:
         }
     """
 
-    def adminGetTable(self, username: str):
-        data, err = None, None
-        data = self.db.Query("Report", None)
-        if not data:
-            err = "查询报表失败"
+    def adminGetTable(self, username):
+        data, err = [{
+            "time": "今日",
+            "totalChargeCount": 30,
+            "totalChargeTime": 99999999,
+            "totalChargeQuantity": 320,
+            "totalChargeCost": 1100,
+            "totalServiceCost": 600,
+            "totalCost": 1700,
+            "chargers": [{
+                "chargerID": 1,
+                "totalChargeCount": 10,
+                "totalChargeTime": 12345678,
+                "totalChargeQuantity": 120,
+                "totalChargeCost": 100,
+                "totalServiceCost": 100,
+                "totalCost": 200
+            }, {
+                "chargerID": 2,
+                "totalChargeCount": 20,
+                "totalChargeTime": 87654321,
+                "totalChargeQuantity": 200,
+                "totalChargeCost": 1000,
+                "totalServiceCost": 500,
+                "totalCost": 1500
+            }]
+        }, {
+            "time": "本周",
+            "totalChargeCount": 30,
+            "totalChargeTime": 99999999,
+            "totalChargeQuantity": 320,
+            "totalChargeCost": 1100,
+            "totalServiceCost": 600,
+            "totalCost": 1700,
+            "chargers": [{
+                "chargerID": 1,
+                "totalChargeCount": 10,
+                "totalChargeTime": 12345678,
+                "totalChargeQuantity": 120,
+                "totalChargeCost": 100,
+                "totalServiceCost": 100,
+                "totalCost": 200
+            }, {
+                "chargerID": 2,
+                "totalChargeCount": 20,
+                "totalChargeTime": 87654321,
+                "totalChargeQuantity": 200,
+                "totalChargeCost": 1000,
+                "totalServiceCost": 500,
+                "totalCost": 1500
+            }]
+        }
+                    ], None
         return data, err
